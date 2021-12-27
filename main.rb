b79fb916809489deb3c0733cbb20deb135696e97
Koji Ishimoto <koji.ishimoto@gmail.com>
2021-12-27 12:24:24 +0900

    add routes and erb files

--- main.rb
+++ main.rb
@@ -5,6 +5,18 @@ get '/' do
   erb :index
 end
 
+get '/new' do
+  erb :new
+end
+
+get '/edit' do
+  erb :edit
+end
+
+get '/show' do
+  erb :show
+end
+
 get '/hello/*' do |name|
   "hello #{name}. how are you?"
 end
